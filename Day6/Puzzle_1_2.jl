d65cc4d019e6d916b8b3d4764306f6d792cdc419
Vikas Negi <vikas.negi10@gmail.com>
2021-12-14 23:17:53 +0100

    Day7 part-2 is OK + mem. opt. Day6 part-2

--- Day6/Puzzle_1_2.jl
+++ Day6/Puzzle_1_2.jl
@@ -6,7 +6,7 @@ function get_initial_fish(input_file::String)
     line = split(lines[1], ",")
 
     # Initial number of fish
-    fish_num = map(x -> parse(Int128, x), line)
+    fish_num = map(x -> parse(Int8, x), line)
 
     return fish_num
 end
@@ -18,30 +18,57 @@ function get_final_fish(input_file::String, days::Int64)
     for day = 1:days
 
         # Check if there is a fish with timer 0
-        indices_0 = findall(x -> x == 0, fish_num)
-        indices_not0 = findall(x -> x != 0, fish_num)
+        indices_0    = BigInt.(findall(x -> x == 0, fish_num))
+        indices_not0 = BigInt.(findall(x -> x != 0, fish_num))
 
         # Reduce all by 1 when no 0's are found
         if isempty(indices_0)
-            fish_num = fish_num .- 1
+            fish_num = fish_num .- Int8(1)
         else
             # Reduce non-0's by 1
-            fish_num[indices_not0] = fish_num[indices_not0] .- 1
+            fish_num[indices_not0] = fish_num[indices_not0] .- Int8(1)
 
             # Split 0 into a 6 and 8
-            fish_num[indices_0] .= 6
+            fish_num[indices_0] .= Int8(6)
 
             for i = 1:length(indices_0)
-                push!(fish_num, 8)
+                push!(fish_num, Int8(8))
             end
         end
 
+        # Free memory
+        indices_0    = 0
+        indices_not0 = 0
+
     end
 
+    fish_total = length(BigInt.(fish_num))    
 
-    return @info "After $(days) days, we will have $(length(fish_num)) fish"
+    return @info "After $(days) days, we will have $(fish_total) fish"
 end
 
+# Part-2
 
+function get_final_big_fish(input_file::String, days::Int64)
 
+    fish_num = get_initial_fish(input_file)
 
+    for day = 1:days
+
+        for i = 1:length(BigInt.(fish_num))
+
+            if fish_num[i] != Int8(0)
+                fish_num[i] = fish_num[i] - Int8(1)
+            else
+                fish_num[i] = Int8(6)
+                push!(fish_num, Int8(8))
+            end
+
+        end
+    end
+
+    fish_total = length(BigInt.(fish_num))
+    
+    return @info "After $(days) days, we will have $(fish_total) fish"
+
+end
--- Day7/Puzzle_1_2.jl
+++ Day7/Puzzle_1_2.jl
@@ -33,3 +33,32 @@ function get_least_fuel(input_file::String)
     return @info "Minimum $(min_fuel) fuel used to align on $(min_target)"
 
 end
+
+# Part-2
+
+function get_least_fuel_new(input_file::String)
+
+    hpos_all = get_hpos(input_file)
+
+    hpos_min, hpos_max = minimum(hpos_all), maximum(hpos_all)
+
+    hpos_range = range(hpos_min, step = 1, stop = hpos_max)
+
+    fuel_total = Int64[]
+
+    for target in hpos_range
+
+        fuel_change = hpos_all .- target
+        fuel_change_abs = abs.(fuel_change)
+
+        # Calculate fuel cost with new rate
+        fuel_change_cost = map(x -> sum(collect(1:x)), fuel_change_abs)
+        push!(fuel_total, sum(fuel_change_cost))
+
+    end
+
+    min_fuel, min_target = findmin(fuel_total)[1], hpos_range[findmin(fuel_total)[2]]
+
+    return @info "Minimum $(min_fuel) fuel used to align on position $(min_target)"
+
+end
