a5dcf8d5d3fd0972d72cb4adf6c8d99b26b33f58
Vikas Negi <vikas.negi10@gmail.com>
2021-12-22 00:33:01 +0100

    Day12 part-1 in progress

--- Day12/Puzzle_1_2.jl
+++ Day12/Puzzle_1_2.jl
@@ -5,15 +5,59 @@ function get_path_connections(input_file::String)
 
     for line in lines
         parts = split(line, "-")
-        push!(connections, (parts[1], parts[2]))
+        push!(connections, [parts[1], parts[2]])
     end
 
     return connections
 end
 
-connections = get_path_connections("Test_input_P1_P2.txt")
+function get_all_paths(input_file::String)
 
-start_paths = filter(x -> "start" in x, connections)
-end_paths = filter(x -> "end" in x, connections)
+    connections = get_path_connections(input_file)
 
+    start_paths = filter(x -> "start" in x, connections)
+    end_paths = filter(x -> "end" in x, connections)
 
+    rest_paths = Any[]
+
+    for path in connections
+        if ~("start" in path) && ~("end" in path)
+            push!(rest_paths, path)
+        end
+    end
+
+    all_paths = Any[]
+
+    for start_path in start_paths
+        for end_path in end_paths
+            for rest_path in rest_paths
+
+                if ~isempty(intersect(start_path, rest_path)) &&
+                ~isempty(intersect(rest_path, end_path))
+
+                push!(all_paths, vcat(start_path, rest_path, end_path))
+
+                end
+
+                if ~isempty(intersect(start_path, end_path))
+
+                    push!(all_paths, vcat(start_path, end_path))
+
+                end
+
+            end
+        end
+    end
+
+    return unique(all_paths)
+end
+
+#=all_rest_paths = Any[]
+
+for i in eachindex(rest_paths)
+
+    if ~isempty(intersect(rest_paths[i:i+1]...)) && i < length(rest_paths)
+
+        push!(vcat(rest_paths[i:i+1]...))
+
+    end=#
