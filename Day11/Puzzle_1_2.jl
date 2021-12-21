3b0b4d3ce5bfe7c743faef90643f8514b5bcdc2d
VIKAS NEGI <vikas.negi10@gmail.com>
2021-12-21 21:28:10 +0100

    Day11 part-2 is OK

--- Day11/Puzzle_1_2.jl
+++ Day11/Puzzle_1_2.jl
@@ -119,3 +119,59 @@ function get_total_flashes(input_file::String, steps::Int64)
 
     return sum(all_flashes)
 end
+
+# Part-2
+
+function get_sync_step(input_file::String)
+
+    e = get_energy_matrix(input_file)
+
+    nrows, ncols = size(e)
+    find_sync = true
+    steps = 0
+
+    while find_sync
+
+        e = e .+ 1
+        flash_search = true
+
+        steps += 1
+        
+        while flash_search
+
+            flash_locs = findall(x -> x > 9, e)
+
+            if ~isempty(flash_locs)
+
+                for loc in flash_locs
+                    r, c = loc[1], loc[2]
+                    e[r, c] = 0
+
+                    adj_loc = get_adjacent_locations(r, c, nrows, ncols)
+
+                    for pos in adj_loc                         
+                        i, j = pos[1], pos[2]
+
+                        # Flashed positions should remain at 0, so update only non-0 positions
+                        if e[i, j] != 0
+                            e[i, j] += 1
+                        end                       
+                    end
+                end
+
+            else
+                @info "No flashes found, moving on to next step!"
+                break
+            end
+
+        end        
+
+        # Number of flashes = Number of O's
+        num_flashes = length(findall(x -> x == 0, e))
+        if num_flashes == nrows * ncols
+            break
+        end        
+    end
+
+    return steps
+end
